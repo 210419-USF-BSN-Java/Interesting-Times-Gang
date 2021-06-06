f1c59b4e17b5b26824cd444f90084b22513eba66
uri-oma <omamalinuriah@gmail.com>
2021-06-05 23:54:19 -0500

    fixed random image api call

--- src/app/components/dashboard/dashboard.component.ts
+++ src/app/components/dashboard/dashboard.component.ts
@@ -12,15 +12,16 @@ import { Image } from '../../models/image';
 export class DashboardComponent implements OnInit {
   constructor(private http: HttpClient) { }
   img?: Image;
+  imgA!: Array<Image>;
   url = 'http://localhost:8080/image/random/?userId=2'
   ngOnInit(): void {
-    this.http.get<Image>(this.url)
-    .subscribe((data: Image) => this.img = data);
-    console.log(this.img);
+    this.http.get<Array<Image>>(this.url)
+    .subscribe((data: Array<Image>) => this.imgA = data);
+    this.img = this.imgA[0];
    }
   showImage() {
-    this.http.get<Image>(this.url)
-      .subscribe((data: Image) => this.img = data);
-      console.log(this.img);
+    this.http.get<Array<Image>>(this.url)
+      .subscribe((data: Array<Image>) => this.imgA = data);
+      this.img = this.imgA[0];
   }
 }
