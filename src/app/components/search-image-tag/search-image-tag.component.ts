9f50d43fad38df1683360ba800703203d1de8c56
uri-oma <omamalinuriah@gmail.com>
2021-06-06 19:54:11 -0500

    separated random image functionality

---
+++ src/app/components/search-image-tag/search-image-tag.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+import { Image } from '../../models/image';
+import { ApiServiceService } from '../../services/api-service.service';
+
+@Component({
+  selector: 'app-search-image-tag',
+  templateUrl: './search-image-tag.component.html',
+  styleUrls: ['./search-image-tag.component.css']
+})
+export class SearchImageTagComponent implements OnInit {
+  tag = '';
+  imgObservable!: Array<Image>;
+  imgA?: Array<Image>;
+  ngOnInit() { }
+  constructor(private apiService: ApiServiceService) { }
+  search() {
+    console.log(this.tag);
+  }
+}
